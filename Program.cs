fc8e0ffeeffe2545a549f8fb3ab6e17f86625baa
KunvarG <kunvar.gayatrik@gmail.com>
2024-02-21 18:55:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myfirstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+        Fizzbuzz.Run();   
+
+            
+        
+        }
+    }
+}
